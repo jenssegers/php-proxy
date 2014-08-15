7cbd0d79debc52dacb95425d719801cae6682c70
Ota Mares <o.mares@rebuy.de>
2014-08-15 16:38:59 +0200

    Basic unittest structure

---
+++ tests/Proxy/ProxyTest.php
@@ -0,0 +1,16 @@
+<?php
+namespace Proxy;
+
+use PHPUnit_Framework_TestCase;
+
+class ProxyTest extends PHPUnit_Framework_TestCase
+{
+    /**
+     * @test
+     */
+    public function stubTest()
+    {
+        $this->assertTrue(true);
+    }
+
+}
